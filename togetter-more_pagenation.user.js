5600d21cc9fc1dbb420942c5d28439362c55a5a0
vzvu3k6k <vzvu3k6k@gmail.com>
2014-06-19 09:23:50 +0900

    Add page separators

--- togetter-more_pagenation.user.js
+++ togetter-more_pagenation.user.js
@@ -24,18 +24,36 @@ window.addEventListener('click', function(event){
   xhr.responseType = 'document';
   xhr.addEventListener('load', function(xhrEvent){
     var insertPoint = event.target.parentNode.nextElementSibling.nextElementSibling;
-    appendPage(xhr.responseXML, insertPoint);
+
+    var hr = document.createElement('hr');
+    hr.setAttribute('class', 'togetter-more_pagenation_page_separator');
+    var p = document.createElement('togetter-more_pagenation_page_info');
+    p.textContent = 'page: ';
+    var a = document.createElement('a');
+    a.setAttribute('class', 'togetter-more_pagenation_page_info');
+    a.setAttribute('href', url);
+    var match;
+    if(match = url.match(/(&|\?)page=(\d+)/)){
+      a.textContent = match[1];
+    }else{
+      a.textContent = '1';
+    }
+    p.appendChild(a);
+
+    var newTweetBoxChildren = Array.prototype.slice.call(
+      xhr.responseXML.querySelector('.tweet_box').children, 0);
+
+    appendPage([hr, p].concat(newTweetBoxChildren), insertPoint);
     location.href = 'javascript:void $.lazy()';
   });
   xhr.send(null);
 }, true);
 
-function appendPage(nextPage, insertPoint){
-  var newTweetBox = nextPage.querySelector('.tweet_box');
-  var tweetBox = document.querySelector('.tweet_box');
+function appendPage(newNodes, insertPoint){
+  var parent = insertPoint.parentNode;
   var c;
-  while(c = newTweetBox.firstElementChild){
-    tweetBox.insertBefore(c, insertPoint.nextSibling);
+  while(c = newNodes.shift()){
+    parent.insertBefore(c, insertPoint.nextSibling);
     insertPoint = c;
   }
 }
